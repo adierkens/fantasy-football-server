281120f218e340c613d710b386740f0dfbd6f149
Adam Dierkens <adam@dierkens.com>
2016-04-04 16:55:47 -0400

    Add post request support for searching for players

--- index.js
+++ index.js
@@ -1,19 +1,54 @@
 'use strict';
 var express = require('express');
 var fetch = require('./src/fetch');
+var bodyparser = require('body-parser');
 
 var app = express();
 var port = process.env.PORT || 8888;
-var router = express.Router();
+app.use(bodyparser.json());
 
-router.get('/players', function(req, res) {
+var allowCrossDomain = function(req, res, next) {
+  res.header('Access-Control-Allow-Origin', '*');
+  res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE');
+  res.header('Access-Control-Allow-Headers', 'Content-Type');
+  res.header('Content-Type', 'application/json');
+  next();
+};
+app.use(allowCrossDomain);
+
+app.get('/players', function(req, res) {
   console.log('Getting players');
   fetch.players(function(players) {
     res.send(players);
   });
 });
 
-router.get('/player/:playerID', function(req, res) {
+app.post('/player', function(req, res) {
+
+  if (Object.keys(req.body).length < 1) {
+    res.send({
+      status: 'error',
+      data: 'Must specify one or more fields to filter on'
+    });
+    return;
+  }
+
+  fetch.player(req.body, function(players) {
+    if (players) {
+      res.send({
+        status: 'success',
+        data: players
+      });
+    } else {
+      res.send({
+        status: 'error',
+        data: 'No players found'
+      });
+    }
+  });
+});
+
+app.get('/player/:playerID', function(req, res) {
   fetch.player(req.params.playerID, function(player) {
     if (player) {
       res.send({
@@ -29,13 +64,13 @@ router.get('/player/:playerID', function(req, res) {
   });
 });
 
-router.get('/teams', function(req, res) {
+app.get('/teams', function(req, res) {
   fetch.teams(function(teams) {
     res.send(teams);
   })
 });
 
-router.get('/team/:teamID', function(req, res) {
+app.get('/team/:teamID', function(req, res) {
   fetch.team(req.params.teamID, function(team) {
     if (team) {
       res.send({
@@ -51,7 +86,6 @@ router.get('/team/:teamID', function(req, res) {
   });
 });
 
-app.use('/', router);
 app.listen(port, function() {
   console.log('Server listening on port ' + port);
 });
--- src/fetch.js
+++ src/fetch.js
@@ -3,13 +3,22 @@ var database = require('./database');
 
 var player = function(playerID, callback) {
   // Look in the DB for a player with that ID
-  database.player( { _id: playerID }, function(docs) {
+
+  var search = playerID;
+
+  if (typeof search === 'string') {
+    search = {
+      _id: playerID
+    }
+  }
+
+  database.players(search, function(docs) {
     callback(docs);
   });
 };
 
 var team = function(teamID, callback) {
-  database.team( { _id: teamID }, function(docs) {
+  database.teams( { _id: teamID }, function(docs) {
     callback(docs);
   });
 };
